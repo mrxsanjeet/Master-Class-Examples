58222e4222e40b6b2cae52bcb4024070c353e937
MarkBrummel <brummelds@gmail.com>
2017-03-21 13:08:09 +0100

    NAV2017

--- PowerShell/SplitFile.ps1
+++ PowerShell/SplitFile.ps1
@@ -2,4 +2,4 @@
 
 import-module "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\NavModelTools.ps1"
 
-Split-NAVApplicationObjectFile -Source "Q:\MasterClass\Exercise 3\Exercise 3.txt" -Destination "Q:\MasterClass\Exercise 3\" -Force
+Split-NAVApplicationObjectFile -Source "Q:\MasterClass\Exercise 4\Exercise 4.txt" -Destination "Q:\MasterClass\Exercise 4\" -Force
