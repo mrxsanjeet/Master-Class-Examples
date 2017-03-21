75ea1721c36d9bb111f8aeb6e1ffe9a19197b5b8
MarkBrummel <brummelds@gmail.com>
2017-03-21 12:50:27 +0100

    NAV 2017

---
+++ PowerShell/SplitFile.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy unrestricted
+
+import-module "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\NavModelTools.ps1"
+
+Split-NAVApplicationObjectFile -Source "Q:\MasterClass\Exercise 2\Exercise 2.txt" -Destination "Q:\MasterClass\Exercise 2\" -Force
